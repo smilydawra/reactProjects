c919397697471c4c83108d8fe565a5bb08a847f5
smilydawra <smilydawra92@gmail.com>
2021-03-19 21:42:45 -0400

    intial setup done

---
+++ lazy-loading-infinity-scroll/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Card from './components/Card';
+
+function App() {
+  return (
+    <div class="container m-4">
+      <h1>List View</h1>
+      <Card />
+    </div>
+  );
+}
+
+export default App;
